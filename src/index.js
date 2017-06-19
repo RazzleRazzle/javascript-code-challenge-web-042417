cc057af961761b7d917c80070104df12d0b8aac1
Jake Musich <makejusich@gmail.com>
2017-06-19 10:33:26 -0400

    Oh, ya, donezo.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 $(document).ready(function(){
-  // your code here!
+  class Comment {
+    constructor(text) {
+      this.text = text
+    }
+
+    render () {
+      return(`<li>${this.text}</li>`)
+    }
+  }
+
+  class CommentList {
+    constructor () {
+      this.comments = []
+    }
+
+    renderComments () {
+      return this.comments.map( comment => comment.render())
+    }
+
+    addComment (text) {
+      const comment = new Comment(text)
+      this.comments.push(comment)
+    }
+
+    render () {
+      return this.renderComments().join('')
+    }
+  }
+
+  class App {
+    constructor() {
+      this.myContainer = document.querySelector("#comment-list")
+      this.myButton = document.querySelector("#button")
+      this.myText = document.querySelector('.form-field')
+      this.commentList = new CommentList()
+      this.myButton.addEventListener('click', this.goGoGo.bind(this))
+    }
+
+    goGoGo() {
+      event.preventDefault()
+      const myInput = this.myText.value
+      this.commentList.addComment(myInput)
+      this.render()
+    }
+
+    render() {
+      this.myContainer.innerHTML = this.commentList.render()
+    }
+  }
+
+  const JakesDopeCommentList = new App()
+
+  JakesDopeCommentList.render()
 })
